9c506ecbc28fbc2d202b377164f7b072085ab3ef
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-19 16:34:23 -0600

    Add items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,20 +1,22 @@
 class Api::V1::ItemsController < ApplicationController
 
   def index
-    render json: Item.all
+    render json: ItemSerializer.new(Item.all)
   end
 
   def show
-    render json: Item.find(params["id"])
+    render json: ItemSerializer.new(Item.find(params[:id]))
   end
 
   def create
-    render json: Item.create(item_params)
+    ActiveRecord::Base.connection.reset_pk_sequence!('items')
+    Item.create(item_params)
+    render json: ItemSerializer.new(Item.last)
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+    params.permit(:name, :description, :unit_price, :merchant_id)
   end
 end
