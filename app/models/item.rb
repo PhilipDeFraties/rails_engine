31ffa074087b92dcb5a0e8024955fa78861acca1
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-19 16:34:57 -0600

    Add item model with import method

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,13 @@ class Item < ApplicationRecord
   def self.import_items
     items = []
     CSV.foreach('db/seeds/items.csv', headers: true) do |row|
+      row["unit_price"] = convert_price(row["unit_price"])
       items << row.to_h
     end
     Item.import(items)
+  end
+
+  def self.convert_price(price)
+    (price.to_i * 0.01).round(2)
   end
 end
