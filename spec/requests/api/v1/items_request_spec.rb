330c18447fadedac84f6d7597a766db6a021f4f2
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-19 21:25:16 -0600

    Add test for item update request

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -96,4 +96,18 @@ describe "Items API" do
     expect(Item.count).to eq(0)
     expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
   end
+
+  it "can update an item" do
+    id = create(:item).id
+    previous_name = Item.last.name
+    item_params = { name: "Kick Pants" }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate(item_params)
+    item = Item.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq("Kick Pants")
+  end
 end
