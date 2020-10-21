6af42a7edd4cf63e12ab3f6e1cabe22992c5c17e
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-21 13:30:52 -0600

    Remove items action

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -22,12 +22,6 @@ class Api::V1::MerchantsController < ApplicationController
     render json: MerchantSerializer.new(Merchant.update(params[:id], merchant_params))
   end
 
-  def items
-    items = Merchant.find(params["merchant_id"]).items
-    render json: ItemSerializer.new(items)
-    # render json: Merchant.find(params["merchant_id"]).items
-  end
-
   private
 
   def merchant_params
