06b06e88dcad69149003beb429bd934e23223405
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-21 14:37:17 -0600

    Add find_all and find routes for merchants resources

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
         resource :merchants, only: %i[show], controller: 'items/merchants'
       end
       resources :merchants do
+        match 'find_all', via: %i[get], as: :find_all, on: :collection
+        match 'find', via: %i[get], as: :find, on: :collection
         resources :items, only: %i[index], controller: 'merchants/items'
       end
     end
