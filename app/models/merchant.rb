e2551e8f5fad1fb7fc672e5f98e399076708054f
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-21 17:40:26 -0600

    Remove search method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,12 +8,4 @@ class Merchant < ApplicationRecord
     end
     Merchant.import(merchants)
   end
-
-  def self.search(query)
-    if query.values.first.blank?
-      all
-    else
-      where("#{query.keys.first} LIKE ?", "%#{query.values.first.downcase}%")
-    end
-  end
 end
