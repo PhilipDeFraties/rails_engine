79a73a567e0065e5573ff43f1ee6d1b75af27cee
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-21 13:31:56 -0600

    Add inherit from BaseSerializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,4 @@
-class ItemSerializer
-  include FastJsonapi::ObjectSerializer
+class ItemSerializer < BaseSerializer
   attributes :name, :description, :unit_price, :merchant_id
   belongs_to :merchant
 end
