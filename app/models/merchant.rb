a71c619fa9e860cfb9983edf01c5cddee904d376
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-21 14:37:35 -0600

    Add search to merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,4 +8,12 @@ class Merchant < ApplicationRecord
     end
     Merchant.import(merchants)
   end
+
+  def self.search(query)
+    if query.values.first.blank?
+      all
+    else
+      where("#{query.keys.first} LIKE ?", "%#{query.values.first.downcase}%")
+    end
+  end
 end
