7c6acbc32c824082b1a585d1e581bac88b850a3c
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-21 14:36:26 -0600

    Add find and find_all actions with private search params

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -22,9 +22,23 @@ class Api::V1::MerchantsController < ApplicationController
     render json: MerchantSerializer.new(Merchant.update(params[:id], merchant_params))
   end
 
+  def find_all
+    merchants = Merchant.search(search_params)
+    render json: MerchantSerializer.new(merchants)
+  end
+
+  def find
+    merchant = Merchant.search(params["name"]).first
+    render json: MerchantSerializer.new(merchants)
+  end
+
   private
 
   def merchant_params
     params.permit(:name)
+  end
+
+  def search_params
+    params.permit(params.keys.first, params.values.first)
   end
 end
